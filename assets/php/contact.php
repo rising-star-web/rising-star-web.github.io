03849650de1e1f3e56e9ca8eed234f62504ee0e7
Robert Balatbat <73982521+robjgb@users.noreply.github.com>
2023-03-19 02:08:25 -0700

    update homepage content, t&c, privacy policy, contact us

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -15,7 +15,7 @@ require 'PHPMailer/src/SMTP.php';
 // Recipients
 $fromEmail = 'from@example.com'; // Email address that will be in the from field of the message.
 $fromName = 'From Name'; // Name that will be in the from field of the message.
-$sendToEmail = 'to@example.com'; // Email address that will receive the message with the output of the form
+$sendToEmail = 'jonwoods321@gmail.com'; // Email address that will receive the message with the output of the form
 $sendToName = 'To Name'; // Name that will receive the message with the output of the form
 
 // Subject
