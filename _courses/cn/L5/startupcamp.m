4cce33c681470d88bb53a74a26309beb45066776
yujiazhang715 <81319388+yujiazhang715@users.noreply.github.com>
2023-12-19 12:31:04 -0800

    Create startupcamp.m

---
+++ _courses/cn/L5/startupcamp.m
@@ -0,0 +1,87 @@
+---
+layout: course_detail
+courseId: ""
+courseIconURL: "TBD - not used yet"
+price: "TBD - not used yet"
+extra: "TBD - not used yet"
+name: "Startup 创业冬令营"
+coverImageURL: "img/updated/enrichment/startup.jpg"
+gradeLevel: "L5 竞技编程"
+pathway: "冬令营"
+skillLevel: "高级"
+shortDescription : "技术创新与创业：从想法到原型"
+description: "下面是一个为期9天的创业冬令营时间安排，旨在指导学生从选择问题痛点到完成技术开发，并最终制作出一个创业原型并准备参加竞赛："
+videoIntro : ""
+learningGoals:
+- "第1天：开营与问题识别
+上午
+开营仪式
+导师介绍与团队分组
+下午
+识别和讨论当前社会、技术领域的热点问题
+引导学生思考潜在的创业机会"
+- "第2天：问题痛点选择与市场调研
+
+全天
+团队讨论确定项目主题
+进行市场调研和需求分析
+初步制定解决方案"
+- "第3天：技术开发基础
+
+上午
+专业培训：基础编程、产品设计等
+下午
+继续培训与实践操作
+晚上：项目计划讨论"
+- "第4天：解决方案设计
+
+全天
+设计思路讨论和确定
+初步设计产品/服务原型
+与导师讨论和修改方案
+"
+- "第5天：技术开发（一）
+
+全天
+开始技术开发工作
+中间阶段检查与导师反馈"
+- "第6天：技术开发（二）
+
+全天
+继续技术开发
+解决开发中的问题和挑战"
+- "第7天：原型制作
+
+全天
+完成原型开发
+测试和优化原型
+"
+- "第8天：项目准备与演练
+
+全天
+准备项目展示和竞赛材料
+演练演示和答辩"
+- "第9天：展示与竞赛
+
+上午
+最终项目展示准备
+下午
+项目展示和竞赛
+颁奖仪式和闭营"
+promotions:
+- title: ""
+  text: ""
+  imageURL: ""
+- title: ""
+  text: ""
+  imageURL: ""
+- title: ""
+  text: ""
+  imageURL: ""
+- title: ""
+  text: ""
+  imageURL: ""
+- title: ""
+  text: ""
+  imageURL: ""
+---
